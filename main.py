1ecc7204db8d999b7bbdbc7261df9c942b740442
23Edu4rd0 <eduardo.vchaves@outlook.com>
2025-04-28 19:24:34 -0300

    Replace icon path with a new icon file

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from os_app import OrdemServicoApp
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.iconbitmap(r'assets\icons8-o-um-anel-40.ico')
+    root.iconbitmap('icon.ico')
     app = OrdemServicoApp(root)
     root.mainloop()
