4b37e1badfc32ea0b2f3cde4d55f02ccc9d64e84
23Edu4rd0 <eduardo.vchaves@outlook.com>
2025-04-28 19:13:51 -0300

    Update icon path to relative reference

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from os_app import OrdemServicoApp
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.iconbitmap(r'C:\Users\eduar\OneDrive\Área de Trabalho\Projeto_Shayder_Final\assets\icons8-o-um-anel-40.ico')
+    root.iconbitmap(r'assets\icons8-o-um-anel-40.ico')
     app = OrdemServicoApp(root)
     root.mainloop()
