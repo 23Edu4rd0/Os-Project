35b41827a0fdb667b399f86e6a3a119775cece14
23Edu4rd0 <eduardo.vchaves23@gmail.com>
2025-05-17 00:21:29 -0300

    fix: indentation in main.py

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ Módulo principal que inicia a aplicação de Ordem de Serviço (OS).
 Este script inicializa a interface gráfica usando ttkbootstrap e
 executa a aplicação principal definida em `os_app.py`.
 
-Lembrar de criar uma area de imposto e um area no tk para colocar o valor
 """
 
 import ttkbootstrap as tb
 from os_app import OrdemServicoApp
 
-if __name__ == "__main__": 
-    root = tb.Window(themename="darkly")  # Temas possíveis: darkly, flatly, cyborg.
-    app = OrdemServicoApp(root)           
+if __name__ == "__main__":
+    root = tb.Window(themename="darkly")
+    app = OrdemServicoApp(root)
     root.mainloop()
